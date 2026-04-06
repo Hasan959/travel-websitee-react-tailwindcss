63149335302dee2a202c48b1ab72903147efc260
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2026-04-06 23:59:27 +0600

    jjdf

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,1 +1,11 @@
-import React, {createContext} from "react";
+import React, { createContext, useState} from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({children}) => {    //children mane er ei provider er moddhe jotogulo component rakha hbe(cart,product,etc) sob e children hisabe pabe. 
+    const [cartItems, setCartItems ] = useState(() => {
+        const stored = localStorage.getItem('cart')
+        return stored ? JSON.parse(stored) : [];kkk
+    })
+    
+}
