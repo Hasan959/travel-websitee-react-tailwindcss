1778ea23a22d6b8228f2296f0c6d963d5275e14d
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2026-04-07 23:05:45 +0600

    cartcontext update

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,11 +1,22 @@
-import React, { createContext, useState} from "react";
+import React, { createContext, useEffect, useState} from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({children}) => {    //children mane er ei provider er moddhe jotogulo component rakha hbe(cart,product,etc) sob e children hisabe pabe. 
     const [cartItems, setCartItems ] = useState(() => {
         const stored = localStorage.getItem('cart')
-        return stored ? JSON.parse(stored) : [];kkk
+        return stored ? JSON.parse(stored) : [];//kkk
     })
+
+    useEffect(()=> {
+        localStorage.setItem('cart',JSON.stringify(cartItems))
+    },[cartItems])
+
+    const addToCart = (item) => {
+        const exists = cartItems.find(cart => cart.id === item.id)
+         if(!exists){
+            setCartItems(prev =>[...prev ,item] )
+         }
+    }
     
 }
