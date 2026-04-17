35b061dbeb99e578f336516270fa7b4f9c0da714
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2026-04-17 23:58:20 +0600

    updted

--- src/Components/pages/CartContextppp.jsx
+++ src/Components/pages/CartContextppp.jsx
@@ -5,6 +5,7 @@ export const CartContext = createContext()
 export const CartProvider = ({Children}) => {
     const [cartItems,setCartItems ] = useState( () => {
         const stored =localStorage.getItem('cart')
+        const stored =localStorage.getItem('cart')
 
     } )
 }
